c7e8eb5961ff1bd7f0925cd9dc046c7776eadf12
Kamrul Hasan Rayhan <aahim7406@gmail.com>
2022-02-22 01:00:22 +0600

    Update ListedUniversity.js

--- component/small/ListedUniversity.js
+++ component/small/ListedUniversity.js
@@ -22,7 +22,7 @@ function ls({ university, wish = false }) {
       `https://limitless-taiga-11177.herokuapp.com/api/wishlist?user_mail=${email}`
     );
     const wishlist = await wishlistRes.json();
-    console.log(wishlist)
+    console.log(wishlist);
     setWishlists(wishlist);
   }, [dataUpdated]);
 
@@ -36,12 +36,11 @@ function ls({ university, wish = false }) {
       },
     };
     try {
-      const universityRes = await fetch(
+      await fetch(
         `https://limitless-taiga-11177.herokuapp.com/api/wishlist?user_mail=${user_mail}&_id=${_id}`,
         settings
       );
-      const university = await universityRes.json();
-      setdataUpdated(Math.random());
+      await setdataUpdated(Math.random());
       return data;
     } catch (e) {
       return e;
@@ -57,12 +56,11 @@ function ls({ university, wish = false }) {
       },
     };
     try {
-      const universityRes = await fetch(
+      await fetch(
         `https://limitless-taiga-11177.herokuapp.com/api/wishlist?user_mail=${user_mail}&_id=${_id}`,
         settings
       );
-      const university = await universityRes.json();
-      setdataUpdated(Math.random());
+      await setdataUpdated(Math.random());
       return data;
     } catch (e) {
       return e;
