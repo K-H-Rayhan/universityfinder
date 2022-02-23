13cc0d7c505fc06d4e5f4ea08784ec6187de1702
kh-rayhan <97230670+kh-rayhan@users.noreply.github.com>
2022-02-23 18:12:46 +0600

    commit on 'main'

--- pages/ranking.js
+++ pages/ranking.js
@@ -13,12 +13,19 @@ function Ranking({ universities }) {
 
 export default Ranking;
 
-export async function getServerSideProps() {
-  // Fetch events
+// export async function getServerSideProps() {
+//   // Fetch events
 
+//   const universityRes = await fetch(`https://limitless-taiga-11177.herokuapp.com/api/find/`);
+//   const universities = await universityRes.json();
+//   return {
+//     props: { universities: universities},
+//   };
+// }
+export async function getStaticProps() {
   const universityRes = await fetch(`https://limitless-taiga-11177.herokuapp.com/api/find/`);
   const universities = await universityRes.json();
   return {
     props: { universities: universities},
-  };
+  }
 }
